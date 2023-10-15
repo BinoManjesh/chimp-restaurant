84ff4f6bb1fb2a96c41ecf367fdce96f6d9c2259
BinoManjesh <binomanjesh@gmail.com>
2023-10-15 22:09:09 +0530

    Add tabs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import { make, makeText } from './tree_maker'
 
 document.body.appendChild(
     make('div', {'id': 'content'}, [
-        makeText('h1', 'Hello World!')
+        make('div', {id: 'tab-list'}, [
+            makeText('button', 'Home'),
+            makeText('button', 'Menu'),
+            makeText('button', 'Contact'),
+        ]),
+        make('div', {'id': 'tab-content'}, [
+            makeText('p', 'bruh')
+        ])
     ])
 )
